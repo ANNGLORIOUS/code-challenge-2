506ae3153763c121428b89bc6fc3d3d387d20b47
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-08-09 15:52:17 +0300

    initial commit

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import BotCard from "./BotCard"
+import React from 'react';
+import BotCard from "./BotCard";
 
 export default function BotCollection({ botCollection, action, removeCard }) {
   const displayBotCards = botCollection.map(bot => {
-    return <BotCard bot={bot} action={action} removeCard={removeCard} />
-  })
+    return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />;
+  });
 
   return (
-    <div className="ui four column grid">
-      <div className="row">
-        {displayBotCards}
-        You have completed your Bot Army. There are no more bots to collect.
-      </div>
+    <div className="ui grid">
+      {displayBotCards}
+      {botCollection.length === 0 && <p>You have completed your Bot Army. There are no more bots to collect.</p>}
     </div>
-  )
+  );
 }
